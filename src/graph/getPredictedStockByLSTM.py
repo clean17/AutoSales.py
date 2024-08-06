16b020b744936f39f50e5c89c901d10872792541
clean17 <piw940317@gmail.com>
2024-08-07 01:02:49 +0900

    mod getPredictedStockByLSTM.py

--- src/graph/getPredictedStockByLSTM.py
+++ src/graph/getPredictedStockByLSTM.py
@@ -16,41 +16,13 @@ ReLU    - 비선형 학습(복잡한 패턴)
 '''
 
 
-
-
-# 데이터 수집
-today = datetime.today().strftime('%Y%m%d')
-last_year = (datetime.today() - timedelta(days=365)).strftime('%Y%m%d')
-# ticker = "005930"  # 삼성전자
-# ticker = "012750"  # 에스원
-ticker = "248070"
-prediction_period = 7
-
-ohlcv = stock.get_market_ohlcv_by_date(fromdate=last_year, todate=today, ticker=ticker)
-fundamental = stock.get_market_fundamental_by_date(fromdate=last_year, todate=today, ticker=ticker)
-data = pd.concat([ohlcv['종가'], fundamental['PER']], axis=1).dropna()
-
-
-
-
-
-# 데이터 전처리
-# scaler = MinMaxScaler(feature_range=(0, 1))
-# scaled_data = scaler.fit_transform(data)
-
-# look_back = 1
-# X, Y = create_dataset(scaled_data, look_back)
-
-def create_dataset(dataset, look_back=1):
+def create_dataset(dataset, look_back=60):
+    # 시계열 데이터를 윈도우로 나누기, 60: 최근 60일 데이터를 기반으로 예측
     X, Y = [], []
-    for i in range(len(dataset) - look_back - 1):
-        a = dataset[i:(i + look_back), :]
-        X.append(a)
-        Y.append(dataset[i + look_back, 0])
+    for i in range(len(dataset) - look_back):
+        X.append(dataset[i:i+look_back])
+        Y.append(dataset[i+look_back, 3])  # 종가(Close) 예측
     return np.array(X), np.array(Y)
-
-
-
 
 
 # LSTM 모델 생성 및 학습
@@ -67,55 +39,91 @@ def create_model(input_shape):
     model.compile(optimizer='adam', loss='mean_squared_error')
     return model
 
-def train_and_save_model(data, look_back=1, model_path='my_model.Keras'):
+
+def train_and_save_model(data, look_back=60, model_path='my_model.keras'):
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaled_data = scaler.fit_transform(data)
 
     X, Y = create_dataset(scaled_data, look_back)
     model = create_model((X.shape[1], X.shape[2]))
     model.fit(X, Y, batch_size=32, epochs=50, verbose=0, validation_split=0.1)
-    # model.fit(X, Y, batch_size=32, epochs=50) # epochs 학습 횟수 batch_size 학습에 사용할 샘플의 수 # epochs 증가
-    model.save(model_path)
+    model.save(model_path, save_format='keras')
     return scaler
 
-# 모델 학습 및 저장
-scaler = train_and_save_model(data, model_path='my_model.Keras')
 
-
-
-
-
-
-# 모델 불러오기 및 예측
-def predict_stock_price_with_saved_model(data, scaler, model_path='my_model.Keras', look_back=1, days_to_predict=prediction_period):
+# 모델을 사용하여 예측 함수
+def predict_stock_price_with_saved_model(data, scaler, model_path='my_model.keras', look_back=60, days_to_predict=7):
     scaled_data = scaler.transform(data)
 
-    def create_dataset(dataset, look_back=1):
+    def create_prediction_dataset(dataset, look_back=60):
         X = []
-        for i in range(len(dataset) - look_back - 1):
-            a = dataset[i:(i + look_back), :]
-            X.append(a)
+        for i in range(len(dataset) - look_back + 1):
+            X.append(dataset[i:(i + look_back), :])
         return np.array(X)
 
-    X = create_dataset(scaled_data, look_back)
+    X = create_prediction_dataset(scaled_data, look_back)
     model = load_model(model_path)
+
+    # Ensure we have enough data to predict the requested days
+    if len(X) < days_to_predict:
+        raise ValueError(f"Not enough data to predict {days_to_predict} days. Available data points: {len(X)}")
+
     predictions = model.predict(X[-days_to_predict:])
-    return predictions
+    predictions = predictions.reshape(-1, 1)
+
+    # Make sure the dimensions match for concatenation
+    zeros_array = np.zeros((predictions.shape[0], data.shape[1] - 1))
+    predictions_extended = np.concatenate((zeros_array, predictions), axis=1)
+
+    original_predictions = scaler.inverse_transform(predictions_extended)[:, -1]
+
+    return original_predictions
+
+
+
+
+
+
+# 데이터 수집
+today = datetime.today().strftime('%Y%m%d')
+last_year = (datetime.today() - timedelta(days=365)).strftime('%Y%m%d')
+# ticker = "005930"  # 삼성전자
+# ticker = "012750"  # 에스원
+ticker = "000150"
+
+
+# 주요 피처(시가, 고가, 저가, 종가, 거래량) + 재무 지표(PER)
+ohlcv = stock.get_market_ohlcv_by_date(fromdate=last_year, todate=today, ticker=ticker)
+fundamental = stock.get_market_fundamental_by_date(fromdate=last_year, todate=today, ticker=ticker)
+
+# PER 값이 없는 경우 대체 값 사용 (예: 0으로 채우기)
+fundamental['PER'] = fundamental['PER'].fillna(0)
+
+# 주가 데이터와 재무 지표 결합 및 NaN 값 처리
+# data = pd.concat([ohlcv['종가'], fundamental['PER']], axis=1).dropna()
+data = pd.concat([ohlcv, fundamental['PER']], axis=1).dropna()
+
+# 필요한 컬럼 선택 및 NaN 값 처리
+data = data[['시가', '고가', '저가', '종가', '거래량', 'PER']].fillna(0)
+
+# 모델 학습 및 저장
+my_model_path = 'new_model_origin.keras'
+scaler = train_and_save_model(data, model_path=my_model_path)
+
+
 
 # 주가 예측
+prediction_period = 7 # 예측일
 # predictions = model.predict(X[-7:])
-predictions = predict_stock_price_with_saved_model(data, scaler, model_path='my_model.Keras')
-
-
-
+predictions = predict_stock_price_with_saved_model(data, scaler, model_path=my_model_path, look_back=60, days_to_predict=prediction_period)
 
 
 
 # 예측 결과 시각화
 # predicted_prices = scaler.inverse_transform(np.concatenate((predictions, np.zeros((7, X.shape[2] - 1))), axis=1))[:, 0]
-predicted_prices = scaler.inverse_transform(np.concatenate((predictions.reshape(-1, 1), np.zeros((prediction_period, data.shape[1] - 1))), axis=1))[:, 0]
+predicted_prices = scaler.inverse_transform(np.concatenate((np.zeros((predictions.shape[0], data.shape[1] - 1)), predictions.reshape(-1, 1)), axis=1))[:, -1]
 # predicted_prices = scaler.inverse_transform(np.concatenate((predictions.reshape(-1, 1), np.zeros((7, data.shape[1] - 1))), axis=1))[:, 0]
-actual_prices = data['종가'].values[-7:]
+# actual_prices = data['종가'].values[-7:]
 
 # 마지막 실제 가격 데이터 날짜 가져오기
 last_date = ohlcv.index[-1]
@@ -141,56 +149,3 @@ plt.ylabel('Price')
 plt.legend()
 plt.xticks(rotation=45)
 plt.show()
-
-
-################### 모델 비교 ###################
-
-'''
-MSE가 낮을수록 실제와 동일하다
-
-validation_split=0.1: 10% 검증 90% 학습
-
-test_size=0.2, random_state=42 - 모델 학습 검증때만 사용
-
-##############################################
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-
-# 정규화 사용 모델 학습 및 평가
-model_regularized = Sequential([ # 0.021147689425332107
-    LSTM(64, return_sequences=True, input_shape=(X.shape[1], X.shape[2]), kernel_regularizer=l2(0.01)),
-    Dropout(0.2),
-    LSTM(64, return_sequences=False, kernel_regularizer=l2(0.01)),
-    Dropout(0.2),
-    Dense(32, activation='relu'),
-    Dense(1)
-])
-model_regularized.compile(optimizer='adam', loss='mean_squared_error')
-model_regularized.fit(X_train, Y_train, batch_size=32, epochs=50, validation_split=0.1)
-predictions_regularized = model_regularized.predict(X_test)
-mse_regularized = mean_squared_error(Y_test, predictions_regularized)
-
-# 정규화 미사용 모델 학습 및 평가
-model_non_regularized = Sequential([ # 0.005378332129577116
-    LSTM(64, return_sequences=True, input_shape=(X.shape[1], X.shape[2])),
-    Dropout(0.2),
-    LSTM(64, return_sequences=False),
-    Dropout(0.2),
-    Dense(32, activation='relu'),
-    Dense(1)
-])
-model_non_regularized.compile(optimizer='adam', loss='mean_squared_error')
-model_non_regularized.fit(X_train, Y_train, batch_size=32, epochs=50, validation_split=0.1)
-predictions_non_regularized = model_non_regularized.predict(X_test)
-mse_non_regularized = mean_squared_error(Y_test, predictions_non_regularized)
-
-print(f'MSE with regularization: {mse_regularized}')
-print(f'MSE without regularization: {mse_non_regularized}')
-###########################################
-'''
-
-
-# # 모델 학습 후 저장
-# model_64.save('my_model.h5')
-#
-# # 모델 불러오기
-# model = load_model('my_model.h5')
