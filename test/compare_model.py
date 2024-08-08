9aa3640fdb314a75c9f4d9e9d0fd416d93fcb073
clean17 <piw940317@gmail.com>
2024-08-08 21:31:04 +0900

    random number dataset, applying checkpoint

--- test/compare_model.py
+++ test/compare_model.py
@@ -4,8 +4,10 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, Dropout
 import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import ModelCheckpoint
+import os
 
 # 데이터 수집
 ticker = '000150'  # 예시: 000150 종목 코드
@@ -27,36 +29,58 @@ for i in range(len(scaled_data) - sequence_length):
     y.append(scaled_data[i+sequence_length, 3])  # 종가(Close) 예측
 
 X = np.array(X)
-y = np.array(y)
+Y = np.array(y)
 
 # 학습 데이터와 검증 데이터 분리
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 # 모델 정의 및 학습
+# model = Sequential([
+#     LSTM(256, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])),
+#     LSTM(128, return_sequences=True),
+#     LSTM(64, return_sequences=False),
+#     Dense(128),
+#     Dense(64),
+#     Dense(32),
+#     Dense(1)
+# ])
 model = Sequential([
     LSTM(256, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])),
+    Dropout(0.2),  # 드롭아웃 추가
     LSTM(128, return_sequences=True),
+    Dropout(0.2),  # 드롭아웃 추가
     LSTM(64, return_sequences=False),
     Dense(128),
+    Dropout(0.2),  # 드롭아웃 추가
     Dense(64),
     Dense(32),
     Dense(1)
 ])
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(X_train, y_train, batch_size=32, epochs=50, validation_split=0.1)
+
+model_file_path = 'my_model.h5'
+checkpoint = ModelCheckpoint(
+    model_file_path,
+    monitor='val_loss',
+    save_best_only=True,
+    mode='min',
+    verbose=1
+)
+
+model.fit(X_train, Y_train, epochs=50, batch_size=32, verbose=1,
+          validation_data=(X_val, Y_val), callbacks=[checkpoint])
 
 # 모델 저장
-model.save('my_model.h5')
-
+# model.save('my_model.h5')
 # 모델 로드
-model_loaded = load_model('my_model.h5')
+# model_loaded = load_model('my_model.h5') # 체크포인트가 자동으로 저장
 
 # 새로운 데이터 예측
-predictions = model_loaded.predict(X_val)
+predictions = model.predict(X_val)
 
 # 실제 값과 예측 값의 비교
-plt.figure(figsize=(16, 8))
-plt.plot(y_val, label='Actual Price')
+plt.figure(figsize=(24, 10))
+plt.plot(Y_val, label='Actual Price')
 plt.plot(predictions, label='Predicted Price')
 plt.title(f'{ticker} Stock Price Prediction')
 plt.xlabel('Time')
