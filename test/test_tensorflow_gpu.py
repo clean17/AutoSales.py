cbdb06a6aef221793749d4534c9c1d718be7c3f7
inwoo.park <piw940317@gmail.com>
2025-06-09 16:40:41 +0900

    Change to TensorFlow version 2.9.0

--- test/test_tensorflow_gpu.py
+++ test/test_tensorflow_gpu.py
@@ -1,9 +1,47 @@
 import tensorflow as tf
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print('1', tf.config.list_physical_devices('GPU'))
-print('2', tf.test.is_built_with_cuda())
-print('3', tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None))
+gpus = tf.config.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(gpus))
+print("GPU List: ", gpus)
 
+# 추가로, 로컬 디바이스 정보 출력 (디버깅용)
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
+'''
+2025-06-09 11:00:47.795172: I tensorflow/core/platform/cpu_feature_guard.cc:182] This TensorFlow binary is optimized to use available CPU instructions in performance-critical operations.
+To enable the following instructions: SSE SSE2 SSE3 SSE4.1 SSE4.2 AVX AVX2 AVX_VNNI FMA, in other operations, rebuild TensorFlow with the appropriate compiler flags.
+[name: "/device:CPU:0"
+device_type: "CPU"
+memory_limit: 268435456
+locality {
+}
+incarnation: 18292826920800437369
+xla_global_id: -1
+]
+
+# tensorflow 2.9.0 버전 설치 시
+
+2025-06-09 15:49:53.245135: I tensorflow/core/platform/cpu_feature_guard.cc:193] This TensorFlow binary is optimized with oneAPI Deep Neural Network Library (oneDNN) to use the following CPU instructions in performance-critical operations:  AVX AVX2
+To enable them in other operations, rebuild TensorFlow with the appropriate compiler flags.
+2025-06-09 15:49:53.743461: I tensorflow/core/common_runtime/gpu/gpu_device.cc:1532] Created device /device:GPU:0 with 3425 MB memory:  -> device: 0, name: NVIDIA GeForce RTX 4050 Laptop GPU, pci bus id: 0000:01:00.0, compute capability: 8.9
+[name: "/device:CPU:0"
+device_type: "CPU"
+memory_limit: 268435456
+locality {
+}
+incarnation: 7484648450752542634
+xla_global_id: -1
+, name: "/device:GPU:0"
+device_type: "GPU"
+memory_limit: 3591372800
+locality {
+  bus_id: 1
+  links {
+  }
+}
+incarnation: 7534614764908712837
+physical_device_desc: "device: 0, name: NVIDIA GeForce RTX 4050 Laptop GPU, pci bus id: 0000:01:00.0, compute capability: 8.9"
+xla_global_id: 416903419
+]
+
+'''
