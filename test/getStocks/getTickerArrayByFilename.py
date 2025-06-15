30467c6a5f96966843a39c75a6cda55570d53481
clean17 <piw940317@gmail.com>
2025-06-15 13:51:56 +0900

    modify getTickerArrayByFilename.py

--- test/getStocks/getTickerArrayByFilename.py
+++ test/getStocks/getTickerArrayByFilename.py
@@ -14,8 +14,31 @@ def extract_numbers_from_filenames(directory):
 
     return numbers
 
-# directory = 'D:\kosdaq_stocks'
-directory = 'D:\kospi_stocks'
 
-extracted_numbers = extract_numbers_from_filenames(directory)
-print("Extracted numbers:", extracted_numbers)
+def extract_stock_code_from_filenames(directory):
+    stock_codes = []
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.png'):
+            # .png 떼고 strip
+            name_only = os.path.splitext(filename)[0].strip()
+            # 만약 ']'로 끝나면 스킵 (삼륭물산 [014970] 처럼)
+            if name_only.endswith(']'):
+                continue
+            # ']' 뒤에 나오는 영어/숫자만 추출 (공백 포함 가능)
+            match = re.search(r'\]\s*([A-Za-z0-9]+)$', name_only)
+            if match:
+                stock_codes.append(match.group(1))
+
+    return stock_codes
+
+
+directory = r'D:\kospi_stocks'  # 역슬래시 r''로 표기
+
+
+# extracted_numbers = extract_numbers_from_filenames(directory)
+# print("Extracted numbers:", extracted_numbers)
+
+
+extracted_last_chars = extract_stock_code_from_filenames(directory)
+print("Extracted last chars:", extracted_last_chars)
