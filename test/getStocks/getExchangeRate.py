afb3ed502ea7f39a190511c0901944cf90ee91b8
clean17 <piw940317@gmail.com>
2025-07-01 12:31:17 +0900

    Modify the function to import the won-dollar exchange rate

--- test/getStocks/getExchangeRate.py
+++ test/getStocks/getExchangeRate.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_usd_krw_rate():
-    url = "https://finance.naver.com/marketindex/exchangeDetail.naver?marketindexCd=FX_USDKRW"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    res = requests.get(url, headers=headers)
-    soup = BeautifulSoup(res.text, "html.parser")
-    target_em = soup.select_one('p.no_today em.no_up em.no_up')
-    if target_em:
-        rate_str = ''.join([span.get_text() for span in target_em.find_all('span')])
-        # 쉼표 제거, float 변환
-        return float(rate_str.replace(',', ''))
-    else:
-        print('em.no_up em.no_up이 없습니다.')
+def get_usd_krw_rate(url="https://m.search.naver.com/p/csearch/content/qapirender.nhn?key=calculator&pkid=141&q=%ED%99%98%EC%9C%A8&where=m&u1=keb&u6=standardUnit&u7=0&u3=USD&u4=KRW&u8=down&u2=1"):
+    response = requests.get(url)
+    data = response.json()
+    # "country" 리스트에서 currencyUnit이 "원"인 항목 찾기
+    for item in data.get("country", []):
+        if item.get("currencyUnit") == "원":
+            # 콤마(,) 제거 후 float 변환
+            value_str = item.get("value", "0").replace(',', '')
+            return float(value_str)
+    # 못 찾으면 None 반환
+    return None
+
 
 rate = get_usd_krw_rate()
 print("현재 원/달러 환율:", rate)
