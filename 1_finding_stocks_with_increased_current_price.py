9b2bd7a5b8c35b609d83b1ce838e07c0d85a1219
clean17 <piw940317@gmail.com>
2025-09-06 22:00:10 +0900

    Check the list of stock items on the server

--- 1_finding_stocks_with_increased_current_price.py
+++ 1_finding_stocks_with_increased_current_price.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from pykrx import stock
 from datetime import datetime, timedelta
-from utils import create_lstm_model, create_multistep_dataset, fetch_stock_data, add_technical_features, get_kor_ticker_list, check_column_types, get_safe_ticker_list
+from utils import fetch_stock_data, add_technical_features
 import unicodedata
 import requests
 
@@ -23,7 +23,11 @@ AVERAGE_TRADING_VALUE = 1_000_000_000 # 평균거래대금
 today = datetime.today().strftime('%Y%m%d')
 start_yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y%m%d')
 
-tickers = get_kor_ticker_list()
+url = "https://chickchick.shop/func/stocks/kor"
+res = requests.get(url)
+data = res.json()
+tickers = [item["stock_code"] for item in data if "stock_code" in item]
+# tickers = get_kor_ticker_list()
 ticker_to_name = {ticker: stock.get_market_ticker_name(ticker) for ticker in tickers}
 
 
@@ -78,6 +82,16 @@ for count, ticker in enumerate(tickers):
     if recent_average_trading_value <= AVERAGE_TRADING_VALUE:
         continue
 
+
+    # 2차 생성 feature
+    data = add_technical_features(data)
+
+    # 현재 5일선이 20일선보다 낮으면서 하락중이면 패스
+    ma_angle_5 = data['MA5'].iloc[-1] - data['MA5'].iloc[-2]
+    if data['MA5'].iloc[-1] < data['MA20'].iloc[-1] and ma_angle_5 < 0:
+        # print(f"                                                        5일선이 20일선 보다 낮을 경우 → pass")
+        continue
+
     ########################################################################
     # ======== 조건 체크 시작 ========
 
@@ -108,15 +122,15 @@ for count, ticker in enumerate(tickers):
 
     try:
         res = requests.post(
-            'http://localhost:8090/func/stocks/info',
+            'https://chickchick.shop/func/stocks/info',
             json={"stock_name": str(ticker)},
             timeout=5
         )
         data = res.json()
-        product_code = data.result[0].data.items[0].productCode;
+        product_code = data["result"][0]["data"]["items"][0]["productCode"]
 
         res2 = requests.post(
-            'http://localhost:8090/func/stocks/overview',
+            'https://chickchick.shop/func/stocks/overview',
             json={"product_code": str(product_code)},
             timeout=5
         )
@@ -138,7 +152,7 @@ for count, ticker in enumerate(tickers):
 
     try:
         requests.post(
-            'http://localhost:8090/func/stocks/interest',
+            'https://chickchick.shop/func/stocks/interest',
             json={
                 "nation": "kor",
                 "stock_code": str(ticker),
--- 2_finding_stocks_with_increased_volume.py
+++ 2_finding_stocks_with_increased_volume.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from pykrx import stock
 from datetime import datetime, timedelta
-from utils import create_lstm_model, create_multistep_dataset, fetch_stock_data, add_technical_features, get_kor_ticker_list, check_column_types, get_safe_ticker_list
+from utils import fetch_stock_data, add_technical_features
 import unicodedata
 import requests
 
@@ -22,7 +22,11 @@ os.makedirs(pickle_dir, exist_ok=True)
 today = datetime.today().strftime('%Y%m%d')
 start_yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y%m%d')
 
-tickers = get_kor_ticker_list()
+url = "https://chickchick.shop/func/stocks/kor"
+res = requests.get(url)
+data = res.json()
+tickers = [item["stock_code"] for item in data if "stock_code" in item]
+# tickers = get_kor_ticker_list()
 ticker_to_name = {ticker: stock.get_market_ticker_name(ticker) for ticker in tickers}
 
 
@@ -112,15 +116,15 @@ for count, ticker in enumerate(tickers):
 
         try:
             res = requests.post(
-                'http://localhost:8090/func/stocks/info',
+                'https://chickchick.shop/func/stocks/info',
                 json={"stock_name": str(ticker)},
                 timeout=5
             )
             data = res.json()
-            product_code = data.result[0].data.items[0].productCode;
+            product_code = data["result"][0]["data"]["items"][0]["productCode"]
 
             res2 = requests.post(
-                'http://localhost:8090/func/stocks/overview',
+                'https://chickchick.shop/func/stocks/overview',
                 json={"product_code": str(product_code)},
                 timeout=5
             )
@@ -140,7 +144,7 @@ for count, ticker in enumerate(tickers):
 
         try:
             requests.post(
-                'http://localhost:8090/func/stocks/interest',
+                'https://chickchick.shop/func/stocks/interest',
                 json={
                     "nation": "kor",
                     "stock_code": str(ticker),
